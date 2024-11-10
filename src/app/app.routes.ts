7ee057da7e5aa587cb1cd8f1a334321939c07d75
EduAG <jesus.e.alvarez@gmail.com>
2024-11-10 01:33:36 -0600

    Some additions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',  // Todo:localhost:4200/ <--
+        loadChildren:  () => import(`./modules/home/home.module`).then(m => m.HomeModule)
+    }
+];
